a6c318b5752a948c475db1880910a0f53b017697
Adrian Meca <adrianmeca@gmail.com>
2015-04-10 17:56:26 -0300

    Agrego constructor en Hijo con herencia

--- ejemplos-clase-2015/EjemploHerencia/src/herencia/Hijo.java
+++ ejemplos-clase-2015/EjemploHerencia/src/herencia/Hijo.java
@@ -1,5 +1,9 @@
 package herencia;
 
-public class Hijo extends Padre{ //extends se utiliza para indicar que Hijo hereda de Padre 
+public class Hijo extends Padre{ //extends se utiliza para indicar que Hijo hereda de Padre
+	public Hijo(){
+		super("Hijo","Hijo");//invoco al constructor padre con parametros
+		//los constructores no se heredan implicitamente
+	}
 
 }
--- ejemplos-clase-2015/EjemploHerencia/src/herencia/Principal.java
+++ ejemplos-clase-2015/EjemploHerencia/src/herencia/Principal.java
@@ -13,8 +13,6 @@ public class Principal {
 		
 		System.out.println("h");
 		Hijo h=new Hijo(); //las clases hijas heredan los métodos de la clase padre
-		h.setClase("Hijo");
-		h.setTipo("Hijo");
 		
 		System.out.println("Clase:"+h.getClase()+"\nTipo:"+h.getTipo());
 	}

8a730ae7fe9bf6ec60743f678e6b3d7c9764a57b
Adrian Meca <adrianmeca@gmail.com>
2015-05-17 19:32:02 -0300

    Resolución Ejercicio 1

---
+++ ejemplos-clase-2015/Clase3/src/general/Menu.java
@@ -0,0 +1,28 @@
+package general;
+
+import java.util.Scanner;
+
+import ejercicios.Ejercicio1;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("Ingrese el número de ejercicio a ejecutar (1, 2 o 3):");
+		Scanner s = new Scanner(System.in);
+		String op = s.nextLine();
+		
+		switch (op) {
+		case "1":
+			new Ejercicio1().ejecutar();
+			break;
+		case "2":
+		
+			break;
+		case "3":
+			
+			break;
+		}
+
+	}
+
+}
